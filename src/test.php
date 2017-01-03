56a7e45d4563d076d041c7aefd6c0a237645e0fb
Keshav Aditya <Keshav Aditya>
2017-01-04 02:30:20 +0530

    root1

---
+++ src/test.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>testing</title>
+
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <link rel="stylesheet" type="text/css" href="css/HomeStyle.css">
+   
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+
+</head>
+<body>
+<div class="container">
+<img src="images/logo.png">
+<form action="example4.php" method="get" class="centered">
+<input placeholder="Search" id="first_name" type="text" class="validate" name="lol">
+<button class="btn waves-effect waves-light" type="submit" name="hit">GO</button>	
+</form>
+
+<section class="contact">
+<div class="container"> 
+<p>Would you like a better deal ? <br> Give us your contact details,& we'd get in touch with you</p>
+<form action="ex1.php" method="get">
+<div class="row">
+<div class="col s4">
+<input placeholder="Name" type="text" class="validate" name="fullName">
+</div>
+<div class="col s4">
+<input placeholder="Phone Number" type="text" class="validate" name="phoneNumber" size="10">
+</div>
+<div class="col s4">
+<input placeholder="Product Name" type="text" class="validate" name="ProductName">
+</div>
+</div>
+<div class="row">
+<div class="col s12">
+<button class="btn waves-effect waves-light" type="submit" name="getUserDetails">Submit</button>
+</div>
+</div>
+</form>
+</div>
+</section>
+</div>
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+
+</body>
+</html>
+
+
